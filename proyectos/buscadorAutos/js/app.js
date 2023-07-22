a439fed10d1b59c0cfc7f8632456a2cc20aceb32
Cristian Pan <a16003566@alumnos.uady.mx>
2023-07-21 18:03:09 -0600

    seccion 17: finalizada
    videos: 148 - 152

--- proyectos/buscadorAutos/js/app.js
+++ proyectos/buscadorAutos/js/app.js
@@ -16,48 +16,113 @@ const datosBusqueda = {
   year: "",
   puertas: "",
   transmision: "",
-  color: "", 
-  minimo: "", 
+  color: "",
+  minimo: "",
   maximo: "",
 };
 
 document.addEventListener("DOMContentLoaded", () => {
-    agregarEventListener();
+  agregarEventListener();
   mostrarAutos();
   llenarSelectAnios();
-
 });
 
-function agregarEventListener(){
-    marca.addEventListener('change', actualizarObjeto)
-    year.addEventListener('change', actualizarObjeto)
-    minimo.addEventListener('change', actualizarObjeto)
-    maximo.addEventListener('change', actualizarObjeto)
-    puertas.addEventListener('change', actualizarObjeto)
-    transmision.addEventListener('change', actualizarObjeto)
-    color.addEventListener('change', actualizarObjeto)
+function agregarEventListener() {
+  marca.addEventListener("change", actualizarObjeto);
+  year.addEventListener("change", actualizarObjeto);
+  minimo.addEventListener("change", actualizarObjeto);
+  maximo.addEventListener("change", actualizarObjeto);
+  puertas.addEventListener("change", actualizarObjeto);
+  transmision.addEventListener("change", actualizarObjeto);
+  color.addEventListener("change", actualizarObjeto);
 }
 
-function actualizarObjeto({target}){
-    const {value, id} = target;
-    datosBusqueda[id] = value; 
-    filtrarAuto(); 
+function actualizarObjeto({ target }) {
+  const { value, id } = target;
+  datosBusqueda[id] = value;
+  filtrarAuto();
 }
 
 //funcion que filtra en base a la busqueda
-function filtrarAuto(){
-    const resultado = autos.filter(filtrarMarca); 
-    limpiarListadoResultado(); 
-    mostrarAutos(resultado); 
+function filtrarAuto() {
+  const resultado = autos
+    .filter(filtrarMarca)
+    .filter(filtrarYear)
+    .filter(filtrarMinimo)
+    .filter(filtrarMaximo)
+    .filter(filtrarPuertas)
+    .filter(filtrarTransmision)
+    .filter(filtrarColor);
+  limpiarListadoResultado();
+
+  if (resultado.length) {
+    mostrarAutos(resultado);
+  } else {
+    mostrarAlerta(); 
+  }
 }
 
-function filtrarMarca(auto){
-    const {marca} = datosBusqueda; 
-    if(datosBusqueda.marca){
-        return auto.marca === marca; 
-    }
+function filtrarMarca(auto) {
+  const { marca } = datosBusqueda;
+  if (marca) {
+    return auto.marca === marca;
+  }
 
-    return auto; 
+  return auto;
+}
+
+function filtrarYear(auto) {
+  const { year } = datosBusqueda;
+  if (year) {
+    return auto.year === parseInt(year);
+  }
+
+  return auto;
+}
+
+function filtrarMinimo(auto) {
+  const { minimo } = datosBusqueda;
+  if (minimo) {
+    return auto.precio >= parseInt(minimo);
+  }
+
+  return auto;
+}
+
+function filtrarMaximo(auto) {
+  const { maximo } = datosBusqueda;
+  if (maximo) {
+    return auto.precio <= parseInt(maximo);
+  }
+
+  return auto;
+}
+
+function filtrarPuertas(auto) {
+  const { puertas } = datosBusqueda;
+  if (puertas) {
+    return auto.puertas === parseInt(puertas);
+  }
+
+  return auto;
+}
+
+function filtrarColor(auto) {
+  const { color } = datosBusqueda;
+  if (color) {
+    return auto.color === color;
+  }
+
+  return auto;
+}
+
+function filtrarTransmision(auto) {
+  const { transmision } = datosBusqueda;
+  if (transmision) {
+    return auto.transmision === transmision;
+  }
+
+  return auto;
 }
 
 function mostrarAutos(arrayAutos = autos) {
@@ -68,7 +133,7 @@ function mostrarAutos(arrayAutos = autos) {
 
     const autoHtml = document.createElement("p");
     autoHtml.textContent = `
-            ${marca} - ${modelo} - ${year} - ${puertas} - ${transmision} - ${precio} - ${color} 
+            ${marca} - ${modelo} - ${year} - ${puertas} - ${transmision} - $${precio} - ${color} 
         `;
 
     fragment.appendChild(autoHtml);
@@ -77,10 +142,10 @@ function mostrarAutos(arrayAutos = autos) {
   resultado.appendChild(fragment);
 }
 
-function limpiarListadoResultado(){
-    while(resultado.firstChild){
-        resultado.removeChild(resultado.firstChild); 
-    }
+function limpiarListadoResultado() {
+  while (resultado.firstChild) {
+    resultado.removeChild(resultado.firstChild);
+  }
 }
 
 function llenarSelectAnios() {
@@ -95,4 +160,9 @@ function llenarSelectAnios() {
   }
 }
 
-
+function mostrarAlerta(){
+  const alerta = document.createElement('p'); 
+  alerta.textContent = "No hay resultados de tu busqueda"; 
+  
+  resultado.appendChild(alerta); 
+}
