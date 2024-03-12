2539ec4063eda6027770fb5df25d68ffe15c05ef
Cristian Pan <a16003566@alumnos.uady.mx>
2024-03-12 09:18:46 -0600

    Seccion 55: avances
    videos: 446 - 460

---
+++ proyectos/cotizador-prestamos-vue/src/App.vue
@@ -0,0 +1,39 @@
+<script setup>
+
+import {ref, computed} from 'vue'; 
+
+import Header from "./components/Header.vue";
+
+const MIN_VALUE = 0; 
+const MAX_VALUE = 20000; 
+const STEP = 100; 
+const amount = ref(10000); 
+
+const getAmountWhitFormat = computed(() => {
+  const formatter = new Intl.NumberFormat('en-US', {
+    style: 'currency', 
+    currency: 'USD'
+  }); 
+
+  return formatter.format(amount.value); 
+})
+</script>
+
+<template>
+  <div class="my-20 max-w-lg mx-auto bg-white shadow-md p-10">
+    <Header />
+
+    <div class="my-5">
+      <input
+        type="range"
+        class="w-full bg-gray-200 accent-lime-500 hover:accent-lime-600"
+        :min="MIN_VALUE"
+        :max="MAX_VALUE"
+        :step="STEP"
+        v-model.number="amount"
+      />
+
+      <p class="text-center my-10 text-5xl font-extrabold text-indigo-600">{{getAmountWhitFormat}}</p>
+    </div>
+  </div>
+</template>
---
+++ proyectos/cotizador-prestamos-vue/src/components/Header.vue
@@ -0,0 +1,5 @@
+<template>
+    <h1 class="text-4xl font-extrabold text-gray-500 text-center">
+        ¿Cuánto <span class="text-indigo-600">Dinero</span> necesitas?
+    </h1>
+</template>
